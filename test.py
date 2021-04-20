7298827b9dedb0a2890bff67abd021efa1527b6a
Erwan Orgiazzi <erwanorg@gmail.com>
2021-04-20 12:06:11 +0200

    test

---
+++ test.py
@@ -0,0 +1,23 @@
+import unittest
+
+import maclasse
+
+class TestClasse(unittest.TestCase):
+
+
+def test_life_objet(self,bird):
+    self.assertIsInstance(bird.gettingolder,int)
+
+def test_life(self,bird) :
+    res=True
+    if bird.age>5:
+        res=False
+    self.assertTrue(res)
+
+def life_object2(self,bird):
+    self.assertIsInstance(bird.life,bool)
+
+
+
+
+
